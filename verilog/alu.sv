f2f0f9c89e76dee13a6a6c2d1c5b783e9eb37668
Akshat Choudhari <88554618+A-Choudhari@users.noreply.github.com>
2026-07-16 12:32:38 -0400

    Refactor ALU module to use localparams and fix assignments

--- verilog/alu.sv
+++ verilog/alu.sv
@@ -1,36 +1,36 @@
 // Design
 // ALU
 module alu (op,
-  num1, num2, result);
-  input     [3:0] op;
+  num1, num2, result, zero);
+  input     [2:0] op;
   input     [3:0] num1;
   input     [3:0] num2;
   output	reg [3:0] result;
   output	reg zero;
 
-  wire ADD = 3'b000;
-  wire SUBTRACT = 3'b001;
-  wire AND = 3'b010;
-  wire OR = 3'b011;
-  wire XOR = 3'b100;
-  wire SLT = 3'b101;
+  localparam ADD = 3'b000;
+  localparam SUBTRACT = 3'b001;
+  localparam AND = 3'b010;
+  localparam OR = 3'b011;
+  localparam XOR = 3'b100;
+  localparam SLT = 3'b101;
 
   always @(*)
     begin
     case(op)
       ADD:
-        result <= num1 + num2;
+        result = num1 + num2;
       SUBTRACT:
-        result <= num1 - num2;
+        result = num1 - num2;
       AND:
-        result <= num1 & num2;
+        result = num1 & num2;
       OR:
-        result <= num1 | num2;
+        result = num1 | num2;
       XOR:
-        result <= num ^ num2;
+        result = num1 ^ num2;
       SLT:
-        result = (a < b) ? 4'b0001 : 4'b0000;
-      default: result = 4'b0000;
+        result = (num1 < num2) ? 4'b0001 : 4'b0000;
+      default: result <= 4'b0000;
     endcase
     zero = (result == 4'b0000);
     end   

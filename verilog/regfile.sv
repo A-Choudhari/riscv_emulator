443f95023aea0966422cdfde22de91aa1fbec1cf
Akshat Choudhari <88554618+A-Choudhari@users.noreply.github.com>
2026-07-17 09:22:07 -0400

    Add register file module in Verilog
    Implemented a register file module with read and write capabilities.

---
+++ verilog/regfile.sv
@@ -0,0 +1,24 @@
+// Design
+// Register File
+module regfile (clk, i_reg1,
+  i_reg2, w_addr, write_enable, write_data, o_reg1, o_reg2);
+  input      clk;
+  input      [2:0]i_reg1;
+  input      [2:0]i_reg2;
+  input		 [2:0]w_addr;
+  input		 write_enable;
+  input		 [3:0]write_data;
+  output     [3:0]o_reg1;
+  output     [3:0]o_reg2;
+
+  reg [3:0] file [0:7];
+  assign o_reg1 = file[i_reg1];
+  assign o_reg2 = file[i_reg2];
+  always @(posedge clk)
+  begin
+    if (write_enable) begin
+      file[w_addr] <= write_data;
+    end
+  end
+  
+endmodule
